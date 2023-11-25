787069c8971e7dff72e98d47e7eca2c6daaef1f1
kamoliddin <justfrozenak@gmail.com>
2023-11-25 08:07:07 +0500

    fixing

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sepkil_shox08'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://epajaiekwfgwnm:2e77952fc4565b6f1d7bab646ccbeffa0727c46f6c0f276e94e2e0da623b747e@ec2-35-169-11-108.compute-1.amazonaws.com:5432/dcbcocisnc82qc'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://epajaiekwfgwnm:2e77952fc4565b6f1d7bab646ccbeffa0727c46f6c0f276e94e2e0da623b747e@ec2-35-169-11-108.compute-1.amazonaws.com:5432/dcbcocisnc82qc'
     db.init_app(app)
 
     from .views import views
